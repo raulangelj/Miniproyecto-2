1adf21d6535152f92f6315976fd8d14067a660bb
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-08-30 17:04:49 -0600

    add: exersice 2.1 completed

--- main.py
+++ main.py
@@ -56,3 +56,30 @@ plt.show()
 
 # %%
 # ejericcio 2.1 Cree un programa que corra el anterior juego del caos y muestre el dibujo resultante
+def f1(x, y):
+  return x*0.85 + y*0.04 + 0.0, x*-0.04 + y*0.85 + 1.6
+def f2(x, y):
+  return -0.15*x + 0.28*y + 0.0, x*0.26 + y*0.24 + 0.44
+def f3(x, y):
+  return x*0.2 + y*-0.26 + 0.0, x*0.23 + y*0.22 + 1.6
+def f4(x, y):
+  return x*0.0 + y*0.0, x*0.0 + y*0.16
+
+F = [f1, f2, f3, f4]
+P = (0.85, 0.07, 0.07, 0.01)
+n = 100000
+# generamos el primer punto aleatorio
+x = random.uniform(0, 1)
+y = random.uniform(0, 1)
+
+# creamos la iteracion de 100,000 veces para dibujar
+for _ in range(n):
+  # dibujamos el punto
+  plt.plot(x, y, marker='o', color='green')
+  # elegimos una funcion aleatoria
+  f = random.choices(F, weights=(0.85, 0.07, 0.07, 0.01), k=1)[0]
+  # calculamos el nuevo punto
+  x, y = f(x, y)
+# mostramos el grafico
+plt.show()
+# %%
